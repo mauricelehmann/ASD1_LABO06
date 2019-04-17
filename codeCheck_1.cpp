2e9fdb821bf1dd8eedf7b10e34db392503afd37e
Maurice <m.lehmann94@gmail.com>
2019-04-17 14:31:06 +0200

    codecheck 1 OK !

--- codeCheck_1.cpp
+++ codeCheck_1.cpp
@@ -1,9 +1,16 @@
 #include "Mouchard.h"
-
+#include <iostream>
 using namespace std;
 
 int main() {
 
-
+    //Expected output : DC IC(5) CC(5) MC(5) C=(5) M=(5) D(-2) D(-1) D(5) D(5)
+    Int a;
+    Int b(5);
+    Int c = b;
+    Int d(move(c));
+    a = b;
+    a = move(d);
+    
     return 0;
 }
