45b2e3889d2a6627d9ced25b5ca213d612760f60
Maurice <m.lehmann94@gmail.com>
2019-04-24 17:20:05 +0200

    code check 3 , marche sur netbeans mais passe pas le codecheck ??

--- codeCheck_3.cpp
+++ codeCheck_3.cpp
@@ -1,9 +1,21 @@
+
 #include "Mouchard.h"
 
 using namespace std;
 
 int main() {
 
+    Int* p = (Int*) ::operator new(4 * sizeof(Int));
 
-    return 0;
+    Int* dcInt = new(p) Int();
+    Int* icInt = new(p + 1) Int(5);
+    Int* ccInt = new(p + 2) Int(*icInt);
+    Int* mcInt = new(p + 3) Int(move(*icInt));
+    *dcInt = *ccInt ;
+    *mcInt = move(*dcInt);
+
+    for (size_t i = 0; i < 4; i++) {
+        delete(p+i) ;
+    }
+    ::operator delete(p);
 }
