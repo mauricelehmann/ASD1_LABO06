efb44500961a91b3810e682e8fcb7ad4a74f1615
Maurice <m.lehmann94@gmail.com>
2019-05-01 13:01:52 +0200

    code check 5 OK ! code check 3 OK !

--- Pile_2.h
+++ Pile_2.h
@@ -2,7 +2,7 @@
 #define Pile_hpp
 
 #include <ostream>
-
+#include <iostream>
 // Forward declarations
 template <typename T> class Pile;
 template <typename T> std::ostream& operator<<( std::ostream& os,
@@ -26,37 +26,57 @@ private:
   const size_t CAPACITE;
 
 public:
-  Pile(size_t _CAPACITE)
-  // a completer
+  Pile(size_t _capacite)
+  : CAPACITE(_capacite), taille(0)
   {
-    // a completer
+    donnees = (pointer) ::operator new(CAPACITE * sizeof(value_type));
   }
 
   ~Pile()
   {
-    // a completer
+    for (int i = 0 ; i < taille; i++) {
+        //On détruit les objets via leur destructeur
+        (donnees+i)->value_type::~value_type();
+    }
+    //On libère la mémoire alouée
+    ::operator delete(donnees);
   }
 
   Pile(const Pile& other)
-  // a completer
+  :CAPACITE(other.CAPACITE), taille(other.taille)
   {
-    // a completer
+    donnees = (pointer) ::operator new(CAPACITE * sizeof(value_type));
+    for (size_t i = 0; i < taille; i++) {
+        pointer newObject = new(donnees+i) value_type(other.donnees[i]);
+    }
   }
 
   void empiler(const value_type& v)
   {
-    // a completer
+    if(taille <= CAPACITE){
+        pointer newObject = new(donnees+taille++) value_type(v);
+    }else{
+        throw "La pile est pleine!\n";
+    }
   }
 
   void depiler()
   {
-    // a completer
+    if(taille > 0){
+        //On detruit l'objet en mémoire via son destructeur
+        (donnees+taille - 1)->value_type::~value_type();
+        //On réduit la taille
+        taille--;
+    }else{
+        throw "La pile est vide!\n";
+    }
   }
 
   const_reference sommet() const
   {
-    // a completer
+    return donnees[taille-1];
   }
+
 };
 
 // ne pas modifier
